b46edbae286aef885648c56e6aa71b05785929b9
Jorge Hernandez <jorgehernandezc3008@gmail.com>
2023-11-15 16:37:06 -0600

    fet,task-list-server,Implementacion de los metodos post,put y delete

--- src/app.js
+++ src/app.js
@@ -5,8 +5,9 @@ const listEditRouter = require("./list-edit-router")
 const port= 3000; 
 
 
+app.use('/listaTareas',listEditRouter);
 app.use('/listaTareas',listViewRouter);
-app.use('/litarTareas',listEditRouter);
+
 
 
 app.listen(port,()=>{
--- src/list-edit-router.js
+++ src/list-edit-router.js
@@ -1,18 +1,63 @@
+const fs= require("fs");
 const express = require("express");
 const router = express.Router();
 
-
+router.use(express.json());
 
 router.post("/crearTarea",(req,res)=>{
-    
+    try {
+        console.log(req.body);
+        const data = JSON.parse(fs.readFileSync('./src/listaTareas.json'));
+        data.push(req.body);
+        fs.writeFileSync('./src/listaTareas.json', JSON.stringify(data, null, 2));
+         res.status(200).send("Tarea Ingresada con exito")
+      } catch (error) {
+        console.error('Error al leer el archivo:', error.message);
+        res.sendStatus(500);
+      }
 });
 
 router.delete("/eliminarTarea/:id",(req,res)=>{
-
+    try {
+        const id = req.params.id;
+        const data = JSON.parse(fs.readFileSync('./src/listaTareas.json'));
+        const indiceABorrar = data.findIndex((element)=>element.id===id);
+        console.log(indiceABorrar)
+if( indiceABorrar !== -1){
+    data.splice(indiceABorrar,1);
+    fs.writeFileSync('./src/listaTareas.json', JSON.stringify(data, null, 2));
+    res.status(200).send("Tarea borrada con exito")
+}       
+      } catch (error) {
+        console.error('Error al leer el archivo:', error.message);
+        res.sendStatus(500);
+      }
 });
 
-router.put("/editarTarea",(req,res)=>{
 
+router.put("/editarTarea",(req,res)=>{
+    try {
+        console.log(req.body);
+        const data = JSON.parse(fs.readFileSync('./src/listaTareas.json'));
+        const id = req.body.id;
+        const elementoAModificar = data.find((element) => element.id === id);
+        if (elementoAModificar) {
+            elementoAModificar.isCompleted = req.body.isCompleted;
+            elementoAModificar.description = req.body.description;
+            try {
+                fs.writeFileSync('./src/listaTareas.json', JSON.stringify(data, null, 2));
+                res.status(200).send('Archivo actualizado con éxito.');
+              } catch (error) {
+                res.status(400).send('Error al escribir en el archivo:', error.message);
+              };
+          } else {
+            console.error('Elemento no encontrado para modificar.');
+          }
+        }
+      catch (error) {
+        console.error('Error al leer el archivo:', error.message);
+        res.sendStatus(500);
+      }
 });
 
 module.exports = router;
